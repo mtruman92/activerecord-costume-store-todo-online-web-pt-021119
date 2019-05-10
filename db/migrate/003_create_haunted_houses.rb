1fbac496cdb89d390cbf8f179cb122b24450736d
Mickae Truman <mtruman@nyit.edu>
2019-05-10 03:06:31 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 
-class HauntedHouses < ActiveRecord::Migration
+class CreateHauntedHouses < ActiveRecord::Migration
   
   def change
     create_table :costumes do |t|
