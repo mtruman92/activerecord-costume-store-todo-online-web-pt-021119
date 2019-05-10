14e3438eba989632ffbe03f6ae9ea6a4695c6e3b
Mickae Truman <mtruman@nyit.edu>
2019-05-10 02:50:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   
   def change
-    create_table :costume do |t|
+    create_table :costumes do |t|
       t.string :name 
       t.string :price
       t.string :size 
