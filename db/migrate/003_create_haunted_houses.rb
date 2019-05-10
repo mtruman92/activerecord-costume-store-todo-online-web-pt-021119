fea7a1acaa007479f05bfe2ffe029296de81970b
Mickae Truman <mtruman@nyit.edu>
2019-05-10 03:03:53 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+
+class HauntedHouses < ActiveRecord::Migration
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.string :price 
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+  end
+end
