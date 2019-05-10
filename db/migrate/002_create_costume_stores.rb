0289f91a0527fb031a7b25be21ac41c5eb85ad95
Mickae Truman <mtruman@nyit.edu>
2019-05-10 03:00:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name 
+      t.string :location
+      t.string :size 
+      t.string :image_url 
+    end
+  end
+end
